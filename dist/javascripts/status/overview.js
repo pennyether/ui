7db7884a0f040e0dccd8986dab65501128c70402
Penny Ether Team <pennyether@users.noreply.github.com>
2018-04-22 08:50:58 +0000

    Topology: 	- inserts "topology" object into global namespace 	- "topology" object lets you easily set values, and options such as flashing About > Overview: 	- works with new Topology object System Overview: 	- flashes when values change 	- shows deltas for numerical values

--- dist/javascripts/status/overview.js
+++ dist/javascripts/status/overview.js
@@ -6,136 +6,146 @@ Loader.waitFor(() => {
 
 Loader.require("comp", "tr", "token", "tm", "monarchy", "dice", "vp")
 .then(function(comp, tr, token, tm, monarchy, dice, vp){
-    // arrows ///////////////////////////////////////////////////
-    util.bindToElement(tr.capitalRaised().then(util.toEthStr), $(".tr-capital-raised"));
-    util.bindToElement(tr.profitsSent().then(util.toEthStr), $(".tr-dividends"));
-    util.bindToElement(token.totalSupply().then(val => util.toEthStr(val, "PENNY")), $(".token-total-supply"));
-    util.bindToElement(token.dividendsCollected().then(util.toEthStr), $(".token-total-collected"));
-    util.bindToElement(tm.totalRewarded().then(util.toEthStr), $(".tm-rewards-paid"))
-
-    // bankrolled amounts
-    tr.capitalLedger().then(ledgerAddr => {
-        util.bindToElement(tr.capitalAllocatedTo([monarchy.address]).then(util.toEthStr), $(".monarchy-bankrolled"));
-        util.bindToElement(tr.capitalAllocatedTo([dice.address]).then(util.toEthStr), $(".dice-bankrolled"));
-        util.bindToElement(tr.capitalAllocatedTo([vp.address]).then(util.toEthStr), $(".vp-bankrolled"));
-        util.bindToElement(tr.capitalAllocatedTo([tm.address]).then(util.toEthStr), $(".tm-bankrolled"));
+    ethUtil.onStateChanged(state => {
+        refreshAll();
     });
 
-    // profits of each game
-    util.bindToElement(monarchy.profitsSent().then(util.toEthStr), $(".monarchy-profits"));
-    util.bindToElement(dice.profitsSent().then(util.toEthStr), $(".dice-profits"));
-    util.bindToElement(vp.profitsSent().then(util.toEthStr), $(".vp-profits"));
+    function refreshAll() {
+        // arrows ///////////////////////////////////////////////////
+        const eth = {type: "eth", showDelta: true, flashOnDelta: true};
+        const penny = {type: "eth", unit: "PENNY", showDelta: true, flashOnDelta: true};
+        const num = {type: "number", showDelta: true, flashOnDelta: true};
+        const str = {flashOnDelta: true};
+        topology.trCapitalRaised(tr.capitalRaised(), eth);
+        topology.trDividends(tr.profitsSent(), eth);
+        topology.trCapitalRaised(tr.capitalRaised(), eth);
+        topology.tokenTotalSupply(token.totalSupply(), penny);
+        topology.tokenTotalCollected(token.dividendsCollected(), eth);
+        topology.tmRewardsPaid(tm.totalRewarded(), eth);
 
-    // task manager arrows
-    [
-        [monarchy.address, $(".monarchy-send-profits")],
-        [dice.address, $(".dice-send-profits")],
-        [vp.address, $(".vp-send-profits")]
-    ].forEach(obj => {
-        const addr = obj[0];
-        const $e = obj[1];
-        tm.sendProfitsReward([addr]).then(arr => {
-            const reward = arr[0];
-            const profits = arr[1];
-            if (profits.lte(0)) {
-                $e.text("Not needed.")
-            } else {
-                $e.text(`${util.toEthStrFixed(reward, 3)} reward`);
-            }
+        // bankrolled amounts
+        tr.capitalLedger().then(ledgerAddr => {
+            topology.monarchyBankrolled(tr.capitalAllocatedTo([monarchy.address]), eth);
+            topology.diceBankrolled(tr.capitalAllocatedTo([dice.address]), eth);
+            topology.vpBankrolled(tr.capitalAllocatedTo([vp.address]), eth);
+            topology.tmBankrolled(tr.capitalAllocatedTo([tm.address]), eth);
         });
-    });
 
-    [
-        ["startMonarchyGameReward", ".monarchy-start-game"],
-        ["refreshMonarchyGamesReward", ".monarchy-end-game"],
-        ["issueDividendReward", ".tr-issue-dividend"]
-    ].forEach(arr => {
-        tm[arr[0]]().then(res => {
-            const reward = res[0];
-            if (reward.lte(0)) $(arr[1]).text("Not needed.");
-            else $(arr[1]).text(`${util.toEthStrFixed(reward, 3)} reward`);
+        // profits of each game
+        topology.monarchyProfits(monarchy.profitsSent(), eth);
+        topology.diceProfits(dice.profitsSent(), eth);
+        topology.vpProfits(vp.profitsSent(), eth);
+        
+        // task manager arrows
+        [
+            [monarchy.address, topology.monarchySendProfits],
+            [dice.address, topology.diceSendProfits],
+            [vp.address, topology.vpSendProfits]
+        ].forEach(obj => {
+            const addr = obj[0];
+            const fn = obj[1];
+            tm.sendProfitsReward([addr]).then(arr => {
+                const reward = arr[0];
+                const profits = arr[1];
+                const rewardStr = profits.lte(0)
+                    ? "Not needed."
+                    : `${util.toEthStrFixed(reward, 3)} reward`
+                fn(rewardStr, str);
+            });
+        });
+
+        [
+            ["startMonarchyGameReward", topology.monarchyStartGame],
+            ["refreshMonarchyGamesReward", topology.monarchyEndGame],
+            ["issueDividendReward", topology.trIssueDividend]
+        ].forEach(arr => {
+            tm[arr[0]]().then(res => {
+                const reward = res[0];
+                const rewardStr = reward.lte(0)
+                    ? "Not needed."
+                    : `${util.toEthStrFixed(reward, 3)} reward`;
+                arr[1](rewardStr, str);
+            })
+        });
+        ///////////////////////////////////////////////////////////////
+
+        // values for each item //////////////////////////////////////
+        // comptroller
+        Promise.all([
+            comp.dateSaleStarted(),
+            comp.wasSaleStarted(),
+            comp.wasSaleEnded()
+        ]).then(arr => {
+            const dateStarted = arr[0];
+            const wasStarted = arr[1];
+            const wasEnded = arr[2];
+            const $e = $(".crowdsale .value");
+            if (dateStarted.equals(0)) {
+                $e.text("No Date Set.");
+                return;
+            } else if (wasEnded) {
+                $e.text("Ended.");
+            } else if (wasStarted) {
+                const dateStr = util.toDateStr(dateStarted, {second: null});
+                $e.text(`Started (${dateStr})`);
+            } else {
+                const dateStr = util.toDateStr(dateStarted, {second: null});
+                $e.text(dateStr);
+            }
         })
-    });
-    ///////////////////////////////////////////////////////////////
 
-    // values for each item //////////////////////////////////////
-    // comptroller
-    Promise.all([
-        comp.dateSaleStarted(),
-        comp.wasSaleStarted(),
-        comp.wasSaleEnded()
-    ]).then(arr => {
-        const dateStarted = arr[0];
-        const wasStarted = arr[1];
-        const wasEnded = arr[2];
-        const $e = $(".crowdsale .value");
-        if (dateStarted.equals(0)) {
-            $e.text("No Date Set.");
-            return;
-        } else if (wasEnded) {
-            $e.text("Ended.");
-        } else if (wasStarted) {
-            const dateStr = util.toDateStr(dateStarted, {second: null});
-            $e.text(`Started (${dateStr})`);
-        } else {
-            const dateStr = util.toDateStr(dateStarted, {second: null});
-            $e.text(dateStr);
-        }
-    })
+        // Treasury
+        topology.trCapAvailable(tr.capital(), eth);
+        topology.trCapNeeded(tr.capitalNeeded(), eth);
+        topology.trProfits(tr.profits(), eth);
+        topology.trNumRequests(tr.numPendingRequests(), eth);
 
-    // Treasury
-    util.bindToElement(tr.capital().then(util.toEthStr), $(".tr-cap-available"));
-    util.bindToElement(tr.capitalNeeded().then(util.toEthStr), $(".tr-cap-needed"));
-    util.bindToElement(tr.profits().then(util.toEthStr), $(".tr-profits"));
-    util.bindToElement(tr.numPendingRequests(), $(".tr-num-requests"));
+        // PENNY
+        topology.tokenIsFrozen(token.isFrozen(), str)
+        topology.tokenDivsReceived(token.dividendsTotal(), eth);
+        topology.tokenDivsCollected(token.dividendsCollected(), eth);
 
-    // PENNY
-    //util.bindToElement(token.totalSupply().then(util.toEthStr), $(".token-total-supply"));
-    util.bindToElement(token.isFrozen(), $(".token-is-frozen"));
-    util.bindToElement(token.dividendsTotal().then(util.toEthStr), $(".token-divs-received"));
-    util.bindToElement(token.dividendsCollected().then(util.toEthStr), $(".token-divs-collected"));
-    
+        // MONARCHY
+        topology.monarchyActiveGames(monarchy.numActiveGames(), num);
+        topology.monarchyEndedGames(monarchy.numEndedGames(), num);
+        topology.monarchyDefinedGames(monarchy.numDefinedGames(), num);
+        topology.monarchyCurProfits(monarchy.profits(), eth);
 
-    // MONARCHY
-    util.bindToElement(monarchy.numActiveGames(), $(".monarchy-active-games"));
-    util.bindToElement(monarchy.numEndedGames(), $(".monarchy-ended-games"));
-    util.bindToElement(monarchy.numDefinedGames(), $(".monarchy-defined-games"));
-    util.bindToElement(monarchy.profits().then(util.toEthStr), $(".monarchy-cur-profits"));
+        // ID
+        topology.diceNumRolls(dice.curId(), num);
+        topology.diceTotalWagered(dice.totalWagered(), eth);
+        topology.diceCurProfits(dice.profits(), eth);
+        Promise.all([
+            dice.curMaxBet(),
+            dice.maxBet()
+        ]).then(arr => {
+            const max = BigNumber.min(arr[0], arr[1]);
+            topology.diceCurMaxBet(max, eth);
+        });
 
-    // ID
-    util.bindToElement(dice.curId(), $(".dice-num-rolls"));
-    util.bindToElement(dice.totalWagered().then(util.toEthStr), $(".dice-total-wagered"));
-    util.bindToElement(dice.profits().then(util.toEthStr), $(".dice-cur-profits"));
-    Promise.all([
-        dice.curMaxBet(),
-        dice.maxBet()
-    ]).then(arr => {
-        const max = util.toEthStr(BigNumber.min(arr[0], arr[1]));
-        $(".dice-cur-max-bet").text(max);
-    })
+        // VP
+        topology.vpNumGames(vp.curId(), num);
+        topology.vpTotalWagered(vp.totalWagered(), eth);
+        topology.vpCurProfits(vp.profits(), eth);
+        Promise.all([
+            vp.curMaxBet(),
+            vp.maxBet()
+        ]).then(arr => {
+            const max = BigNumber.min(arr[0], arr[1]);
+            topology.vpCurMaxBet(max, eth);
+        });
 
-    // VP
-    util.bindToElement(vp.curId(), $(".vp-num-games"))
-    util.bindToElement(vp.totalWagered().then(util.toEthStr), $(".vp-total-wagered"));
-    util.bindToElement(vp.profits().then(util.toEthStr), $(".vp-cur-profits"));
-    Promise.all([
-        vp.curMaxBet(),
-        vp.maxBet()
-    ]).then(arr => {
-        const max = util.toEthStr(BigNumber.min(arr[0], arr[1]));
-        $(".vp-cur-max-bet").text(max);
-    });
-
-    // Task Manager
-    util.bindToElement(ethUtil.getBalance(tm).then(util.toEthStr), $(".tm-balance"))
-    util.bindToElement(tm.issueDividendRewardBips().then(val => {
-        return `${val.div(100).toFixed(2)}%`
-    }), $(".tm-issue-dividend-reward"))
-    util.bindToElement(tm.sendProfitsRewardBips().then(val => {
-        return `${val.div(100).toFixed(2)}%`;
-    }), $(".tm-send-profits-reward"));
-    util.bindToElement(tm.monarchyStartReward().then(util.toEthStr), $(".tm-pa-start-reward"));
-    util.bindToElement(tm.monarchyEndReward().then(util.toEthStr), $(".tm-pa-end-reward"));
-    ///////////////////////////////////////////////////////////////
+        // Task Manager
+        topology.tmBalance(ethUtil.getBalance(tm), eth);
+        topology.tmIssueDividendReward(tm.issueDividendRewardBips().then(val=>{
+            return `${val.div(100).toFixed(2)}%`
+        }), str);
+        topology.tmSendProfitsReward(tm.sendProfitsRewardBips().then(val => {
+            return `${val.div(100).toFixed(2)}%`;
+        }), str);
+        topology.tmPaStartReward(tm.monarchyStartReward(), eth);
+        topology.tmPaEndReward(tm.monarchyEndReward(), eth);
+        ///////////////////////////////////////////////////////////////
+    }
 });
 
